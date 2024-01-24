74037e5436f7e4d0348c8656ff4c23eac42ed8c7
Mahmuda Nizam Subha <mahmudanizamsubha@gmail.com>
2024-01-24 22:35:20 +0600

    Updated files

--- View/login.php
+++ View/login.php
@@ -1,15 +1,15 @@
 <?php include("header.php");?>
 
         <div class="container py-5 m-9">
-            <div class="container rounded px-2 py-2" style="width: 48rem;">
+            <div class="container  px-2 py-2" style="width: 48rem;">
                 <div class="row gx-2  py-2">
                   <div class="col  py-3">
                    <div class="p-3 m-3 " >
-                    <div class="card text-center rounded m-5"  style="height:50vh">
+                    <div class="card text-center rounded m-5 shadow"  style="height:50vh">
                         <div class="card-header rounded">
                             <img src="https://genexinfosys.com/images/Genex-Logo.png?v=1576746127" width=200px style="padding:15px">
                         </div>
-                        <div class="card-body rounded" style="background-color:#782b90;height:18vh;">
+                        <div class="card-body" style="background-color:#782b90;height:18vh;">
                         <form action="" method="POST">
                           <div class="mb-2">
                             <label for="userID" class="form-label">User ID</label><br>
